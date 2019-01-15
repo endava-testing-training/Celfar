5a6a5e2f202dc2fdae546114774ce1a8c20e7a37
Cintia Hetzer <chetzer@velocitypartners.net>
2019-01-15 16:47:33 -0300

    Fix

--- js/bugs2.js
+++ js/bugs2.js
@@ -1,4 +1,4 @@
-$("p#description")[0].innerHTML = "<a href='https://en.wikipedia.org/iki/Celsius'>Celcius</a> to <a href='https://en.wikipedia.org/wiki/Fahrenheit'>Fahrenheit</a> Converter";
+$("p#description")[0].innerHTML = "<a href='https://en.wikipedia.org/wiki/Celsius'>Celcius</a> to <a href='https://en.wikipedia.org/wiki/Fahrenheit'>Fahrenheit</a> Converter";
 
 validators[VAL_ZERO].valid = function(input) {return parseInt(input) > -273;};
 validators = [
